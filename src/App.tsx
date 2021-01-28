a05e6ed27e19af2d520467ebbc7c54435d64ad16
Yile Ou <troy.yile@gmail.com>
2021-01-28 23:00:00 +0000

    isAction & isObservableProp logging before/after

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { action, flow, observable, reaction } from 'mobx';
+import { action, flow, isAction, isObservableProp, observable, reaction } from 'mobx';
 import React from 'react';
 import { Observer } from 'mobx-react-lite';
 
@@ -12,15 +12,12 @@ import { Observer } from 'mobx-react-lite';
 const o = observable({
   value: 'old',
   normalFunction: () => {
-    console.log('this is a normal function');
     return 'this is a normal function' as string;
   },
   actionFunction: action(() => {
-    console.log('this is an action');
     return 'this is an action' as string;
   }),
   flowFunction: flow(function* () {
-    console.log('this is a flow');
     return 'this is a flow' as string;
   }),
   flowResult: '',
@@ -37,29 +34,43 @@ reaction(
 
 // replace action
 setTimeout(action(() => {
-  console.log('reassigning action')
+  console.log('#actionFunction');
+  console.log(`isAction: ${isAction(o.actionFunction)}`)
+  console.log(`isObservableProp: ${isObservableProp(o, 'actionFunction')}`)
+  console.log('* reassigning action *')
   o.actionFunction = action(() => {
-    console.log('this is a new action');
     return 'this is a new action';
   }) // <- works in both MobX 6.0.4 & 6.1.1
+  console.log(`isAction: ${isAction(o.actionFunction)}`)
+  console.log(`isObservableProp: ${isObservableProp(o, 'actionFunction')}`)
+  console.log('––––––');
 }), 1000);
 
-// replace "authored to be" normal function
+// replace authored-to-be normal function
 setTimeout(action(() => {
-  console.log('reassigning normal function')
+  console.log('#normalFunction');
+  console.log(`isAction: ${isAction(o.normalFunction)}`)
+  console.log(`isObservableProp: ${isObservableProp(o, 'normalFunction')}`)
+  console.log('* reassigning normal function *')
   o.normalFunction = () => {
-    console.log('this is a new normal function')
     return 'this is a new normal function';
   } // <- works with Mobx 6.0.4 and triggers <Observer /> update, fails with Mobx 6.1.1
+  console.log(`isAction: ${isAction(o.normalFunction)}`)
+  console.log(`isObservableProp: ${isObservableProp(o, 'normalFunction')}`)
+  console.log('––––––');
 }), 2000);
 
 // replace flow
 setTimeout(action(() => {
-  console.log('reassigning flow');
+  console.log('#flowFunction');
+  console.log(`isAction: ${isAction(o.flowFunction)}`)
+  console.log(`isObservableProp: ${isObservableProp(o, 'flowFunction')}`)
   o.flowFunction = flow(function* () {
-    console.log('this is a new flow');
     return 'this is a new flow';
   }) // <- works with Mobx 6.0.4 and triggers reaction, fails with Mobx 6.1.1
+  console.log('* reassigning flow *');
+  console.log(`isAction: ${isAction(o.flowFunction)}`)
+  console.log(`isObservableProp: ${isObservableProp(o, 'flowFunction')}`)
 }), 3000);
 
 // replace primitive value
